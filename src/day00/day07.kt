befda744c61fa1c1afc82d3899c06b23001b90da
easchner <eric_aschner@hotmail.com>
2023-12-07 02:08:21 -0600

    Day 7

---
+++ src/day00/day07.kt
@@ -0,0 +1,29 @@
+package day00
+
+import readInputString
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    val testInput = readInputString("day00/test")
+    val input = readInputString("day00/input")
+
+    check(part1(testInput) == 0)
+    val timer1 = measureTimeMillis {
+        println(part1(input))
+    }
+    println("Part 1 took $timer1 ms\n")
+
+    check(part2(testInput) == 0)
+    val timer2 = measureTimeMillis {
+        println(part2(input))
+    }
+    println("Part 2 took $timer2 ms")
+}
