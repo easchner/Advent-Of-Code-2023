4b790d0d2311f50930090d414c633ef29c6f5a2b
easchner <eric_aschner@hotmail.com>
2023-12-20 02:46:04 -0600

    Day 19 & 20

---
+++ src/day19/day19.kt
@@ -0,0 +1,140 @@
+package day19
+
+import readInputSpaceDelimited
+import readInputString
+import java.lang.Integer.max
+import java.lang.Integer.min
+import kotlin.system.measureTimeMillis
+
+data class Rule(val xmas: Char, val greater: Boolean, val number: Int, val state: String)
+data class State(val name: String, val rules: List<Rule>)
+data class Part(val x: Int, val m: Int, val a: Int, val s: Int, var state: String)
+data class PartRange(val min: Int, val max: Int)
+data class GhostPart(var x: PartRange, var m: PartRange, var a: PartRange, var s: PartRange, var state: String)
+
+fun main() {
+    fun parseState(input: String): State {
+        val name = input.split("{").first()
+        val rules = input.split("{")[1].dropLast(1).split(",")
+        val default = Rule('x', true, -1, rules.last())
+        return State(name = name, rules =
+            rules.dropLast(1).map { rule ->
+                val xmas = rule.first()
+                val greater = rule[1] == '>'
+                val number = rule.split(Regex("[<>]"))[1].split(":")[0].toInt()
+                val destination = rule.split(":")[1]
+                Rule(xmas, greater, number, destination)
+            }.toMutableList().plus(default)
+        )
+    }
+
+    fun parsePart(input: String): Part {
+        val n = input.split(",").map { it.split("=")[1].split("}")[0].toInt() }
+        return Part(n[0], n[1], n[2], n[3], "in")
+    }
+
+    fun part1(input: List<List<String>>): Long {
+        val states = input[0].map { parseState(it) }
+        val parts = input[1].map { parsePart(it) }
+
+        while (parts.any { !listOf("A", "R").contains(it.state) }) {
+            for (part in parts.filter { !listOf("A", "R").contains(it.state) }) {
+                val state = states.find { it.name == part.state }!!
+                var destination: String? = null
+                for (rule in state.rules) {
+                    destination = if (rule.greater) {
+                        when(rule.xmas) {
+                            'x' -> if (part.x > rule.number) rule.state else null
+                            'm' -> if (part.m > rule.number) rule.state else null
+                            'a' -> if (part.a > rule.number) rule.state else null
+                            's' -> if (part.s > rule.number) rule.state else null
+                            else -> null
+                        }
+                    } else {
+                        when(rule.xmas) {
+                            'x' -> if (part.x < rule.number) rule.state else null
+                            'm' -> if (part.m < rule.number) rule.state else null
+                            'a' -> if (part.a < rule.number) rule.state else null
+                            's' -> if (part.s < rule.number) rule.state else null
+                            else -> null
+                        }
+                    }
+                    if (destination != null) {
+                        part.state = destination
+                        break
+                    }
+                }
+            }
+        }
+
+        return parts.filter { it.state == "A" }.sumOf { it.x.toLong() + it.m + it.a + it.s }
+    }
+
+    fun part2(input: List<List<String>>): Long {
+        val states = input[0].map { parseState(it) }
+        var ghosts = listOf(
+            GhostPart(PartRange(1, 4000), PartRange(1, 4000), PartRange(1, 4000), PartRange(1, 4000), "in")
+        )
+        val accepted = mutableListOf<GhostPart>()
+
+        while (ghosts.isNotEmpty()) {
+            val next = mutableListOf<GhostPart>()
+            for (ghost in ghosts) {
+                val state = states.find { it.name == ghost.state }!!
+                for (rule in state.rules) {
+                    val pr = when (rule.xmas) {
+                        'x' -> ghost.x
+                        'm' -> ghost.m
+                        'a' -> ghost.a
+                        's' -> ghost.s
+                        else -> throw Exception("rule not xmas - pr")
+                    }
+                    // If range has nothing, skip
+                    if (pr.max > pr.min) {
+                        val newGhostRange = if (rule.greater) PartRange(max(rule.number + 1, pr.min), pr.max) else PartRange(pr.min, min(rule.number - 1, pr.max))
+                        val thisGhostNewRange = if (rule.greater) PartRange(pr.min, min(rule.number, pr.max)) else PartRange(max(rule.number, pr.min), pr.max)
+
+                        when(rule.xmas) {
+                            'x' -> {
+                                next.add(GhostPart(newGhostRange, ghost.m, ghost.a, ghost.s, rule.state))
+                                ghost.x = thisGhostNewRange
+                            }
+                            'm' -> {
+                                next.add(GhostPart(ghost.x, newGhostRange, ghost.a, ghost.s, rule.state))
+                                ghost.m = thisGhostNewRange
+                            }
+                            'a' -> {
+                                next.add(GhostPart(ghost.x, ghost.m, newGhostRange, ghost.s, rule.state))
+                                ghost.a = thisGhostNewRange
+                            }
+                            's' -> {
+                                next.add(GhostPart(ghost.x, ghost.m, ghost.a, newGhostRange, rule.state))
+                                ghost.s = thisGhostNewRange
+                            }
+                        }
+                    }
+                }
+            }
+            accepted.addAll(next.filter { it.state == "A" })
+            next.removeAll { it.state == "A" || it.state == "R" }
+            ghosts = next
+        }
+
+        return accepted.sumOf { (it.x.max.toLong() - it.x.min + 1) * (it.m.max - it.m.min + 1) * (it.a.max - it.a.min + 1) * (it.s.max - it.s.min + 1) }
+    }
+
+    val testInput = readInputSpaceDelimited("day19/test")
+    val input = readInputSpaceDelimited("day19/input")
+
+    check(part1(testInput) == 19114L)
+    val timer1 = measureTimeMillis {
+        println(part1(input))
+    }
+    println("Part 1 took $timer1 ms\n")
+
+    check(part2(testInput) == 167409079868000L)
+    val timer2 = measureTimeMillis {
+        println(part2(input))
+    }
+    println("Part 2 took $timer2 ms")
+}
---
+++ src/day20/day20.kt
@@ -0,0 +1,164 @@
+package day20
+
+import lcm
+import readInputString
+import kotlin.system.measureTimeMillis
+
+data class Module(val label: String, val type: Char, var state: String, var children: List<String>, val parents: MutableList<Pair<String, String>>)
+fun main() {
+    fun parseModule(input: String): Module {
+        val sides = input.split(" -> ")
+        val type = sides[0][0]
+        val label = if (type == 'b') sides[0] else sides[0].drop(1)
+        val children = sides[1].split(", ")
+        return Module(label, type, "low", children, mutableListOf())
+    }
+
+    fun connectChildren(modules: List<Module>) {
+        for (module in modules) {
+            for (child in module.children) {
+                val node = modules.find { it.label == child }
+                node?.parents?.add(Pair(module.label, "low"))
+            }
+        }
+    }
+
+    fun part1(input: List<String>): Long {
+        val modules = input.map { parseModule(it) }
+            .plus(Module("output", 'o', "low", emptyList(), mutableListOf()))
+            .plus(Module("rx", 'o', "low", emptyList(), mutableListOf()))
+        connectChildren(modules)
+
+        var lows = 0L
+        var highs = 0L
+        for (i in 0 until 1000) {
+            val active = mutableListOf(Triple("button", "broadcaster", "low"))
+            while (active.isNotEmpty()) {
+                val currentSignal = active.removeFirst()
+                if (currentSignal.third == "low") lows++ else highs++
+                val currentModule = modules.find { it.label == currentSignal.second }!!
+                when(currentModule.type) {
+                    '%' -> {
+                        if (currentSignal.third == "low") {
+                            currentModule.state = if (currentModule.state == "low") "high" else "low"
+                            for (child in currentModule.children) {
+                                active.add(Triple(currentModule.label, child, currentModule.state))
+                            }
+                        }
+                    }
+                    '&' -> {
+                        val parent = currentModule.parents.find { it.first == currentSignal.first }!!
+                        currentModule.parents.remove(parent)
+                        currentModule.parents.add(Pair(parent.first, currentSignal.third))
+                        val signalOut = if (currentModule.parents.any { it.second == "low" }) "high" else "low"
+                        for (child in currentModule.children) {
+                            active.add(Triple(currentModule.label, child, signalOut))
+                        }
+                    }
+                    'b' -> {
+                        for (child in currentModule.children) {
+                            active.add(Triple(currentModule.label, child, currentSignal.third))
+                        }
+                    }
+                }
+            }
+        }
+
+        return lows * highs
+    }
+
+    fun pruneChildren(modules: List<Module>, keep: String): List<Module> {
+        val broadcaster = modules.find { it.label == "broadcaster" }!!
+        val newBroadcaster = Module(broadcaster.label, 'b', "low", listOf(keep), mutableListOf())
+        val newModules = mutableSetOf(newBroadcaster)
+        var count = 0
+        while (count != newModules.size) {
+            count = newModules.size
+            for (module in newModules.toList()) {
+                for (c in module.children) {
+                    newModules.add(modules.find { it.label == c }!!)
+                }
+            }
+        }
+        return newModules.toList()
+    }
+
+    fun part2(input: List<String>): Long {
+        val allModules = input.map { parseModule(it) }
+            .plus(Module("output", 'o', "low", emptyList(), mutableListOf()))
+            .plus(Module("rx", 'o', "low", emptyList(), mutableListOf()))
+
+        val broadcaster = allModules.find { it.label == "broadcaster" }!!
+        // broadcaster -> fm, hv, kc, bv
+        val lcms = mutableListOf<Long>()
+        for (cycle in broadcaster.children) {
+            val modules = pruneChildren(allModules, cycle)
+            for (module in modules) {
+                module.parents.clear()
+            }
+            connectChildren(modules)
+
+            var count = 0L
+            var found = false
+            while (!found) {
+                count++
+                val active = mutableListOf(Triple("button", "broadcaster", "low"))
+                var rx = 0
+                while (active.isNotEmpty()) {
+                    val currentSignal = active.removeFirst()
+                    if (currentSignal.second == "rx" && currentSignal.third == "low")
+                        rx = 1
+                    val currentModule = modules.find { it.label == currentSignal.second }!!
+                    when (currentModule.type) {
+                        '%' -> {
+                            if (currentSignal.third == "low") {
+                                currentModule.state = if (currentModule.state == "low") "high" else "low"
+                                for (child in currentModule.children) {
+                                    active.add(Triple(currentModule.label, child, currentModule.state))
+                                }
+                            }
+                        }
+
+                        '&' -> {
+                            val parent = currentModule.parents.find { it.first == currentSignal.first }!!
+                            currentModule.parents.remove(parent)
+                            currentModule.parents.add(Pair(parent.first, currentSignal.third))
+                            val signalOut = if (currentModule.parents.any { it.second == "low" }) "high" else "low"
+                            for (child in currentModule.children) {
+                                active.add(Triple(currentModule.label, child, signalOut))
+                            }
+                        }
+
+                        'b' -> {
+                            for (child in currentModule.children) {
+                                active.add(Triple(currentModule.label, child, currentSignal.third))
+                            }
+                        }
+                    }
+                }
+                if (rx == 1) {
+                    lcms.add(count)
+                    found = true
+                }
+            }
+        }
+        return lcm(lcms)
+    }
+
+    val testInput1 = readInputString("day20/test1")
+    val testInput2 = readInputString("day20/test2")
+    val input = readInputString("day20/input")
+
+    check(part1(testInput1) == 32000000L)
+    check(part1(testInput2) == 11687500L)
+    val timer1 = measureTimeMillis {
+        println(part1(input))
+    }
+    println("Part 1 took $timer1 ms\n")
+
+//    check(part2(testInput2) == 0L)
+    val timer2 = measureTimeMillis {
+        println(part2(input))
+    }
+    println("Part 2 took $timer2 ms")
+}
